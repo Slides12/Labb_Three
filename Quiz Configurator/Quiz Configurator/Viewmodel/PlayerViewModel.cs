1f2b3d9a4343df13ed3eeb319260c7c5bd4388bd
Slides12 <djohan93@hotmail.com>
2024-11-02 14:36:30 +0100

    Managed to set the correct answer green if you choose the wrong answer.

--- Quiz Configurator/Quiz Configurator/Viewmodel/PlayerViewModel.cs
+++ Quiz Configurator/Quiz Configurator/Viewmodel/PlayerViewModel.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Web;
 using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Threading;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 using System.Windows.Controls;
@@ -19,7 +18,10 @@ namespace Quiz_Configurator.Viewmodel
     {
         private readonly MainWindowViewModel mainWindowViewModel;
 
-        public DelegateCommand CheckButtonCommand { get; }
+        public DelegateCommand CheckButtonCommand1 { get; }
+        public DelegateCommand CheckButtonCommand2 { get; }
+        public DelegateCommand CheckButtonCommand3 { get; }
+        public DelegateCommand CheckButtonCommand4 { get; }
         private int secondsOnly;
         private bool canPress = true;
 
@@ -79,63 +81,63 @@ namespace Quiz_Configurator.Viewmodel
             }
         }
 
-        private string _correctQuestion;
-        public string CorrectQuestion
+        private string _question1;
+        public string Question1
         {
             get
             {
-                return _correctQuestion;
+                return _question1;
             }
             set
             {
-                _correctQuestion = value;
+                _question1 = value;
 
-                RaiseProperyChanged("CorrectQuestion");
+                RaiseProperyChanged("Question1");
             }
         }
 
-        private string _wrongQuestion1;
-        public string WrongQuestion1
+        private string _question2;
+        public string Question2
         {
             get
             {
-                return _wrongQuestion1;
+                return _question2;
             }
             set
             {
-                _wrongQuestion1 = value;
+                _question2 = value;
 
-                RaiseProperyChanged("WrongQuestion1");
-            }
-        }
-        
-        private string _wrongQuestion2;
-        public string WrongQuestion2
-        {
-            get
-            {
-                return _wrongQuestion2;
-            }
-            set
-            {
-                _wrongQuestion2 = value;
-
-                RaiseProperyChanged("WrongQuestion2");
+                RaiseProperyChanged("Question2");
             }
         }
         
-        private string _wrongQuestion3;
-        public string WrongQuestion3
+        private string _question3;
+        public string Question3
         {
             get
             {
-                return _wrongQuestion3;
+                return _question3;
             }
             set
             {
-                _wrongQuestion3 = value;
+                _question3 = value;
 
-                RaiseProperyChanged("WrongQuestion3");
+                RaiseProperyChanged("Question3");
+            }
+        }
+        
+        private string _question4;
+        public string Question4
+        {
+            get
+            {
+                return _question4;
+            }
+            set
+            {
+                _question4 = value;
+
+                RaiseProperyChanged("Question4");
             }
         }
         
@@ -155,7 +157,80 @@ namespace Quiz_Configurator.Viewmodel
         }
 
 
+        private string _correctQuestion;
+        public string CorrectQuestion
+        {
+            get
+            {
+                return _correctQuestion;
+            }
+            set
+            {
+                _correctQuestion = value;
 
+                RaiseProperyChanged("CorrectQuestion");
+            }
+        }
+
+        private SolidColorBrush _questionColor1;
+        public SolidColorBrush QuestionColor1
+        {
+            get
+            {
+                return _questionColor1;
+            }
+            set
+            {
+                _questionColor1 = value;
+
+                RaiseProperyChanged("QuestionColor1");
+            }
+        }
+
+        private SolidColorBrush _questionColor2;
+        public SolidColorBrush QuestionColor2
+        {
+            get
+            {
+                return _questionColor2;
+            }
+            set
+            {
+                _questionColor2 = value;
+
+                RaiseProperyChanged("QuestionColor2");
+            }
+        }
+
+        private SolidColorBrush _questionColor3;
+        public SolidColorBrush QuestionColor3
+        {
+            get
+            {
+                return _questionColor3;
+            }
+            set
+            {
+                _questionColor3 = value;
+
+                RaiseProperyChanged("QuestionColor3");
+            }
+        }
+
+        private SolidColorBrush _questionColor4;
+        public SolidColorBrush QuestionColor4
+        {
+            get
+            {
+                return _questionColor4;
+            }
+            set
+            {
+                _questionColor4 = value;
+
+                RaiseProperyChanged("QuestionColor4");
+            }
+        }
 
 
 
@@ -165,38 +240,166 @@ namespace Quiz_Configurator.Viewmodel
         public PlayerViewModel(MainWindowViewModel? mainWindowViewModel)
         {
             this.mainWindowViewModel = mainWindowViewModel;
-            CheckButtonCommand = new DelegateCommand(CheckButton);
+            CheckButtonCommand1 = new DelegateCommand(CheckButton1);
+            CheckButtonCommand2 = new DelegateCommand(CheckButton2);
+            CheckButtonCommand3 = new DelegateCommand(CheckButton3);
+            CheckButtonCommand4 = new DelegateCommand(CheckButton4);
             SetTimerValue();
         }
 
-        private async void CheckButton(object obj)
+        private async void CheckButton1(object obj)
         {
-            if (obj is System.Windows.Controls.Button button)
+            if (obj is Button button)
             {
                 if (canPress) 
                 { 
                     if((button.Content as TextBlock).Text.Equals(ActivePack.Questions[Index - 1].CorrectAnswer))
                     {
-                        button.Background = new SolidColorBrush(Colors.LightGreen);
+                        QuestionColor1 = new SolidColorBrush(Colors.LightGreen);
                         canPress = false;
                         Points++;
                         await Task.Delay(1500);
-                        button.Background = new SolidColorBrush(Colors.White);
+                        ResetAllColors();
                         NextQuestion();
                         canPress = true;
                     }
                     else
                     {
-                        button.Background = new SolidColorBrush(Colors.IndianRed);
+                        QuestionColor1 = new SolidColorBrush(Colors.IndianRed);
                         canPress = false;
+                        HighlightCorrectAnswer();
                         await Task.Delay(1500);
-                        button.Background = new SolidColorBrush(Colors.White);
+                        ResetAllColors();
                         NextQuestion();
                         canPress = true;
                     }
                 }
             }
         }
+
+        private async void CheckButton2(object obj)
+        {
+            if (obj is Button button)
+            {
+                if (canPress)
+                {
+                    if ((button.Content as TextBlock).Text.Equals(ActivePack.Questions[Index - 1].CorrectAnswer))
+                    {
+                        QuestionColor2 = new SolidColorBrush(Colors.LightGreen);
+                        canPress = false;
+                        Points++;
+                        await Task.Delay(1500);
+                        ResetAllColors();
+                        NextQuestion();
+                        canPress = true;
+                    }
+                    else
+                    {
+                        QuestionColor2 = new SolidColorBrush(Colors.IndianRed);
+                        canPress = false;
+                        HighlightCorrectAnswer();
+                        await Task.Delay(1500);
+                        ResetAllColors();
+                        NextQuestion();
+                        canPress = true;
+                    }
+                }
+            }
+        }
+
+        private async void CheckButton3(object obj)
+        {
+            if (obj is Button button)
+            {
+                if (canPress)
+                {
+                    if ((button.Content as TextBlock).Text.Equals(ActivePack.Questions[Index - 1].CorrectAnswer))
+                    {
+                        QuestionColor3 = new SolidColorBrush(Colors.LightGreen);
+                        canPress = false;
+                        Points++;
+                        await Task.Delay(1500);
+                        ResetAllColors();
+                        NextQuestion();
+                        canPress = true;
+                    }
+                    else
+                    {
+                        QuestionColor3 = new SolidColorBrush(Colors.IndianRed);
+                        canPress = false;
+                        HighlightCorrectAnswer();
+                        await Task.Delay(1500);
+                        ResetAllColors();
+                        NextQuestion();
+                        canPress = true;
+                    }
+                }
+            }
+        }
+
+        private async void CheckButton4(object obj)
+        {
+            if (obj is Button button)
+            {
+                if (canPress)
+                {
+                    if ((button.Content as TextBlock).Text.Equals(ActivePack.Questions[Index - 1].CorrectAnswer))
+                    {
+                        QuestionColor4 = new SolidColorBrush(Colors.LightGreen);
+                        canPress = false;
+                        Points++;
+                        await Task.Delay(1500);
+                        ResetAllColors();
+                        NextQuestion();
+                        canPress = true;
+                    }
+                    else
+                    {
+                        QuestionColor4 = new SolidColorBrush(Colors.IndianRed);
+                        canPress = false;
+                        HighlightCorrectAnswer();
+                        await Task.Delay(1500);
+                        ResetAllColors();
+                        NextQuestion();
+                        canPress = true;
+                    }
+                }
+            }
+        }
+
+        private void HighlightCorrectAnswer()
+        {
+            
+                string correctAnswer = ActivePack.Questions[Index - 1].CorrectAnswer;
+                if (correctAnswer.Equals(Question1))
+                {
+                    QuestionColor1 = new SolidColorBrush(Colors.LightGreen);
+                }
+                else if (correctAnswer.Equals(Question2))
+                {
+                    QuestionColor2 = new SolidColorBrush(Colors.LightGreen);
+                }
+                else if (correctAnswer.Equals(Question3))
+                {
+                    QuestionColor3 = new SolidColorBrush(Colors.LightGreen);
+                }
+                else if (correctAnswer.Equals(Question4))
+                {
+                    QuestionColor4 = new SolidColorBrush(Colors.LightGreen);
+                }
+        }
+
+        private void ResetAllColors()
+        {
+            QuestionColor1 = new SolidColorBrush(Colors.White);
+            QuestionColor2 = new SolidColorBrush(Colors.White);
+            QuestionColor3 = new SolidColorBrush(Colors.White);
+            QuestionColor4 = new SolidColorBrush(Colors.White);
+
+        }
+
+
+
 
         private void NextQuestion()
         {
@@ -283,15 +486,24 @@ namespace Quiz_Configurator.Viewmodel
 
         private void UpdateQuestions()
         {
-            List<string> strings = SetQuestionsRandomly(ActivePack.Questions[Index - 1].CorrectAnswer, ActivePack.Questions[Index - 1].IncorrectAnswers[0], ActivePack.Questions[Index - 1].IncorrectAnswers[1], ActivePack.Questions[Index - 1].IncorrectAnswers[2]);
+            CorrectQuestion = ActivePack.Questions[Index - 1].CorrectAnswer;
+            List<string> strings = SetQuestionsRandomly
+                (
+                CorrectQuestion, 
+                ActivePack.Questions[Index - 1].IncorrectAnswers[0], 
+                ActivePack.Questions[Index - 1].IncorrectAnswers[1], 
+                ActivePack.Questions[Index - 1].IncorrectAnswers[2]
+                );
+
+
 
             if (ActivePack != null && ActivePack.Questions.Count > 0) 
             { 
             Query = ActivePack.Questions[Index - 1].Query;
-            CorrectQuestion = strings[0];
-            WrongQuestion1 = strings[1];
-            WrongQuestion2 = strings[2];
-            WrongQuestion3 = strings[3];
+            Question1 = strings[0];
+            Question2 = strings[1];
+            Question3 = strings[2];
+            Question4 = strings[3];
             }
         }
         public QuestionPackViewModel? ActivePack { get => mainWindowViewModel.ActivePack; }
